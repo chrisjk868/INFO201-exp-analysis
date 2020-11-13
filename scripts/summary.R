4bacfbc39391202e719554cc35b9baa30e3cad23
chrisjk868 <chrisjk868@gmail.com>
2020-11-14 00:01:37 +0800

    added summary scripts

--- scripts/summary.R
+++ scripts/summary.R
@@ -1,13 +1,40 @@
 install.packages("tidyverse")
 library("tidyverse")
 
-co2 <- read.csv('data/export_20201025_1759.csv',stringsAsFactors = FALSE)
+co2 <- read.csv('https://raw.githubusercontent.com/chrisjk868/INFO201-exp-analysis/master/data/export_20201025_1759.CSV?token=ANZXD3POB2V3VVSIPVWL4227V2ITO',stringsAsFactors = FALSE)
 
-long_dataset_co2 <- gather(co2,key = country, value = co2_export,-X)
+long_dataset <- gather(co2,key = country, value = co2_export,-X)
+long_dataset_co2 <- na.omit(long_dataset)
+colnames(long_dataset_co2)[1] <- "year"
 
 # A function that takes in a dataset and returns a list of info about it:
 summary_info <- list()
-summary_info$num_observations <- nrow(my_dataframe)
-summary_info$some_max_value <- my_dataframe %>%
-  filter(some_var == max(some_var, na.rm = T)) %>%
-  select(some_label)
+
+net_co2_export <- long_dataset_co2 %>%
+  group_by(country) %>%
+  summarise(growth = max(co2_export) - min(co2_export))
+
+summary_info$world_net_co2_export <- sum(net_co2_export$growth)
+  
+summary_info$co2_max_recent <- long_dataset_co2 %>%
+  filter(co2_export == max(co2_export)) %>%
+  filter(year == max(year)) %>% 
+  select(country, year, co2_export)
+
+summary_info$co2_min_recent <- long_dataset_co2 %>%
+  filter(co2_export == min(co2_export)) %>%
+  filter(year == max(year)) %>%
+  select(country, year, co2_export)
+
+summary_info$co2_max_rate <- long_dataset_co2 %>%
+  group_by(country) %>%
+  summarise(growth = max(co2_export) - min(co2_export), duration = max(year) - min(year), growth_rate = growth / duration) %>%
+  filter(growth_rate == max(growth_rate)) %>%
+  select(country, growth_rate)
+
+summary_info$co2_min_rate <- long_dataset_co2 %>%
+  group_by(country) %>%
+  summarise(growth = max(co2_export) - min(co2_export), duration = max(year) - min(year), growth_rate = growth / duration) %>%
+  filter(growth_rate == min(growth_rate)) %>%
+  select(country, growth_rate)
+  
