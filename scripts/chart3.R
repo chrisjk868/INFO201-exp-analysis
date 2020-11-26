d881c92b25283c00d1aacf7a112a62cd5020fef0
chrisjk868 <chrisjk868@gmail.com>
2020-11-26 10:46:31 +0800

    fixed chart 3

--- scripts/chart3.R
+++ scripts/chart3.R
@@ -1,13 +1,11 @@
-
 library("tidyr")
 library("dplyr")
 library("ggplot2")
 
-# co2 <- read.csv('https://raw.githubusercontent.com/chrisjk868/INFO201-exp-analysis/master/export_20201025_1759.CSV'
-#                 ,stringsAsFactors = FALSE)
-co2 <- read.csv('export_20201025_1759.csv',stringsAsFactors = FALSE)
+co2 <- read.csv('https://raw.githubusercontent.com/chrisjk868/INFO201-exp-analysis/master/export_20201025_1759.CSV'
+                ,stringsAsFactors = FALSE)
 
-co2_top5<- co2 %>% 
+co2_top<- co2 %>% 
   filter(X == 1960 | X == 1970 | X == 1980 |
            X == 1990 | X == 2000 | X ==2010 | X == 2018) %>%
   gather( key = country_name, value = co2_emission, -X) %>%
@@ -16,10 +14,10 @@ co2_top5<- co2 %>%
   arrange(X)
 
 
-chart3_plot <- ggplot(data = co2_top5)+
-  geom_col( mapping = aes(x = X, y = co2_emission))+
-  geom_text( mapping = aes(label = country_name ),check_overlap = TRUE, size = 3, nudge_y = 150 )
-
-
-
-
+chart3_plot <- ggplot(data = co2_top,mapping = aes(x = X, y = co2_emission)) +
+  geom_col() +
+  geom_text( mapping = aes(label = country_name ),check_overlap = TRUE, 
+             size = 3, nudge_y = 800 ) +
+  labs( title = "Largest CO2 emitter in 1960-2018",
+        y = "Carbon emissions")+
+  scale_x_continuous("Year", breaks = seq(1960,2020, by =5))
